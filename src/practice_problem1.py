1c655c20d689b8a5e4e0dd4889f3fe66de4dccdf
vandensp <vandensp@rose-hulman.edu>
2019-01-07 09:37:19 -0500

    Done

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Samuel VanDenburgh.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -40,8 +40,8 @@ def main():
     # UN-comment tests as you work the problems.
     ###########################################################################
 
-    # run_test_init()
-    # run_test_append_string()
+    #run_test_init()
+    run_test_append_string()
     # run_test_double()
     # run_test_shrink()
     # run_test_double_then_shrink()
@@ -95,7 +95,7 @@ class Box(object):
           :type volume: int
         """
         # ---------------------------------------------------------------------
-        # TODO: 2. Implement and test this function.
+        # Done: 2. Implement and test this function.
         #     See the testing code (below) for more examples.
         # ---------------------------------------------------------------------
         # ---------------------------------------------------------------------
@@ -103,6 +103,14 @@ class Box(object):
         #    DIFFICULTY:      3
         #    TIME ESTIMATE:   5 minutes.
         # ---------------------------------------------------------------------
+
+        if len(contents) > volume:
+            self. volume = volume
+            self.contents = ''
+        else:
+            self.contents = contents
+            self.volume = volume
+
 
     def append_string(self, additional_contents):
         """
@@ -136,7 +144,7 @@ class Box(object):
           :type additional_contents: str
         """
         # ---------------------------------------------------------------------
-        # TODO: 3. Implement and test this function.
+        # Done: 3. Implement and test this function.
         #     See the testing code (below) for more examples.
         # ---------------------------------------------------------------------
         # ---------------------------------------------------------------------
@@ -158,6 +166,18 @@ class Box(object):
         #       Read_this_ONLY_when_asked_Part_2.txt
         #    and complete your work on the problem.
         # ---------------------------------------------------------------------
+        left_over = ''
+        for k in range (self.volume - len(self.contents), len(additional_contents)):
+            left_over = left_over + additional_contents[k]
+
+        number_of_characters_to_append = min(self.volume - len(self.contents),
+                                             len(additional_contents))
+        addition = ''
+        for k in range (number_of_characters_to_append):
+            addition = addition + additional_contents[k]
+        self.contents = self.contents + addition
+
+        return left_over
 
     def double(self):
         """
